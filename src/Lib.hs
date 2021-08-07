0ca0271e5e15c1d8e991e36e28c626a2900753b9
Jappie Klooster <jappieklooster@hotmail.com>
2021-08-07 13:43:22 +0200

    Make blog post about transformers be mtl

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,49 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE FlexibleContexts #-}
+
+-- I made this blogpost compile: https://blog.cofree.coffee/2021-08-05-a-brief-intro-to-monad-transformers/
+-- I claim fair use since I changed it to mtl style.
 module Lib
-  ( libF
+  ( main
   )
 where
 
-import           Control.Monad.IO.Class
+import qualified Data.Map as M
+import qualified Data.HashSet as S
+import Control.Monad.Except
+import Control.Monad.State
+                 --
+data AST a = Leaf a | Node (AST a) (AST a)
+  deriving (Show, Functor, Foldable, Traversable)
 
-libF :: MonadIO m => m ()
-libF = liftIO $ putStrLn "hello, world ss"
+type VariableName = String
+type Variables = S.HashSet VariableName
+
+assignIndexToVariables ::
+  MonadError String m =>
+  MonadState (M.Map VariableName Int) m =>
+  AST VariableName -> Variables -> m (AST Int)
+assignIndexToVariables ast variables = forM ast $ \var -> do
+  unless (var `S.member` variables) $
+    throwError $ "Unknown Variable " <> var
+  cache <- get
+  case M.lookup var cache of
+    Just index -> pure index
+    Nothing -> do
+      let index = M.size cache
+      put $ M.insert var index cache
+      pure index
+
+main :: IO ()
+main =
+  let vars = S.fromList ["a", "b", "c"]
+      ast = Node (Leaf "a") (Node (Leaf "b") (Node (Leaf "a") (Leaf "c")))
+  in do
+    print $ flip evalState mempty $ runExceptT $ assignIndexToVariables ast vars
+    print $ runExcept $ flip evalStateT mempty $ assignIndexToVariables ast vars
+    print (eitherFive, maybeFive)
+
